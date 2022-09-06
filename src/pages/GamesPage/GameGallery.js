bd780c8f0fb33c085c43686028ce5753c4a97148
Bruno <b.b.da.costa@gmail.com>
2022-09-06 15:54:08 -0300

    🚨 FIx react boostrap for version 18

--- src/pages/GamesPage/GameGallery.js
+++ src/pages/GamesPage/GameGallery.js
@@ -1,30 +1,24 @@
 import styles from "./GamesPage.module.css"
 
-const GameGallery = (props) => {
-  const { game } = props
-
-  return (
-    <div>
-      <h2>Gallery</h2>
-      <div className={styles.gallery}>
-        {
-          game
-            .gallery
-            .map(g =>
-              <div key={g.key} className={styles.galleryItem}>
-                <a
-                  href={g.path}
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <img src={g.thumbnail} alt={g.name} />
-                </a>
-              </div>
-            )
-        }
-      </div>
+const GameGallery = ({ game }) => (
+  <div>
+    <h2>Gallery</h2>
+    <div className={styles.gallery}>
+      {game
+        .gallery
+        .map((g, i) =>
+          <div key={i} className={styles.galleryItem}>
+            <a
+              href={g.path}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={g.thumbnail} alt={g.name} />
+            </a>
+          </div>
+        )}
     </div>
-  )
-}
+  </div>
+)
 
 export default GameGallery;
