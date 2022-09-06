4bb0ca202cf86398098997fdee97391c7761a42c
Bruno <b.b.da.costa@gmail.com>
2022-09-06 15:39:05 -0300

    🚨 Remove image warnings

--- src/pages/ResumePage/ResumePage.js
+++ src/pages/ResumePage/ResumePage.js
@@ -17,38 +17,38 @@ import GamesInfo from "./components/GamesInfo";
 
 const ResumePage = () => {
 
-    let { main, workExperiences, certificates, languages, skills, educations, games }
-        = ResumePageService.getContent();
+  let { main, workExperiences, certificates, languages, skills, educations, games }
+    = ResumePageService.getContent();
 
-    return (
-        <div className={styles.resume}>
-            <div className={styles.resumeHelper}>
-                <div className={styles.homeButton}>
-                    <Link to="/">{<FaHome />}</Link>
-                </div>
-                <LocalizationHandler direction={LocalizationDirection.vertical} />
-            </div>
-            <div className={styles.page}>
-                <img src={bgImg} className={styles.bgImg} />
-                <div data-testid="resume-page" style={{ position: "relative", zIndex: 1 }}>
-                    <HeaderInfo main={main} />
-                    <div className={styles.contentBody}>
-                        <div className={styles.sideContent}>
-                            <AdditionalInfo main={main} />
-                            <Languages languages={languages} />
-                            <Skills skills={skills} />
-                            {games && <GamesInfo games={games} />}
-                        </div>
-                        <div className={styles.content}>
-                            <WorkExperiences workExperiences={workExperiences} />
-                            <Certificates certificates={certificates} />
-                            <Education educations={educations} />
-                        </div>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className={styles.resume}>
+      <div className={styles.resumeHelper}>
+        <div className={styles.homeButton}>
+          <Link to="/">{<FaHome />}</Link>
         </div>
-    );
+        <LocalizationHandler direction={LocalizationDirection.vertical} />
+      </div>
+      <div className={styles.page}>
+        <img src={bgImg} className={styles.bgImg} alt="background" />
+        <div data-testid="resume-page" style={{ position: "relative", zIndex: 1 }}>
+          <HeaderInfo main={main} />
+          <div className={styles.contentBody}>
+            <div className={styles.sideContent}>
+              <AdditionalInfo main={main} />
+              <Languages languages={languages} />
+              <Skills skills={skills} />
+              {games && <GamesInfo games={games} />}
+            </div>
+            <div className={styles.content}>
+              <WorkExperiences workExperiences={workExperiences} />
+              <Certificates certificates={certificates} />
+              <Education educations={educations} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ResumePage;
--- src/pages/ResumePage/components/AdditionalInfo.js
+++ src/pages/ResumePage/components/AdditionalInfo.js
@@ -8,50 +8,48 @@ import { ImLocation2 } from 'react-icons/im';
 import Section from "./Section";
 
 const AdditionalInfo = (props) => {
-    let { main } = props;
-    return (
-        <Section>
-            <div className={styles.additionalInfo}>
-                <GrMail />
-                <ButtonMailto
-                    label={main.email}
-                    mailto={`mailto:${main.email}`}
-                />
-            </div>
-            <div className={styles.additionalInfo}>
-                <BsTelephoneFill />
-                <p>{main.phone}</p>
-            </div>
-            <div className={styles.additionalInfo}>
-                <ImLocation2 />
-                <a href={main.locationMaps} target="_blank" rel="noreferrer">
-                    {main.location}
-                </a>
-            </div>
-            <div className={styles.additionalInfo}>
-                <FaLinkedin />
-                <a href={main.linkedin} target="_blank" rel="noreferrer">{main.linkedin}</a>
-            </div>
-            <div className={styles.additionalInfo}>
-                <FaGithub />
-                <a href={main.github} target="_blank" rel="noreferrer">{main.github}</a>
-            </div>
-        </Section>
-    );
+  let { main } = props;
+  return (
+    <Section>
+      <div className={styles.additionalInfo}>
+        <GrMail />
+        <ButtonMailto
+          label={main.email}
+          mailto={`mailto:${main.email}`}
+        />
+      </div>
+      <div className={styles.additionalInfo}>
+        <BsTelephoneFill />
+        <p>{main.phone}</p>
+      </div>
+      <div className={styles.additionalInfo}>
+        <ImLocation2 />
+        <a href={main.locationMaps} target="_blank" rel="noreferrer">
+          {main.location}
+        </a>
+      </div>
+      <div className={styles.additionalInfo}>
+        <FaLinkedin />
+        <a href={main.linkedin} target="_blank" rel="noreferrer">{main.linkedin}</a>
+      </div>
+      <div className={styles.additionalInfo}>
+        <FaGithub />
+        <a href={main.github} target="_blank" rel="noreferrer">{main.github}</a>
+      </div>
+    </Section>
+  );
 }
 
-const ButtonMailto = ({ mailto, label }) => {
-    return (
-        <a style={{ cursor: "pointer", textDecorationLine: "underline", color: "#666" }}
-            to='#'
-            onClick={(e) => {
-                window.location = mailto;
-                e.preventDefault();
-            }}
-        >
-            {label}
-        </a>
-    );
-};
+const ButtonMailto = ({ mailto, label }) => (
+  <span style={{ cursor: "pointer", textDecorationLine: "underline", color: "#666" }}
+    to='#'
+    onClick={(e) => {
+      window.location = mailto;
+      e.preventDefault();
+    }}
+  >
+    {label}
+  </span>
+);
 
 export default AdditionalInfo;
