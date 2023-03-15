6d98b9df08de5bae47059122ab27818c96297706
Bruno <b.b.da.costa@gmail.com>
2023-03-15 15:07:35 -0300

    ✨ Article single responsibility principle

--- src/config/RegisteredArticles.js
+++ src/config/RegisteredArticles.js
@@ -2,7 +2,8 @@ const RegisteredArticles = [
   "unity-timer-tutorial",
   "dice-tutorial-part-1",
   "linear-interpolation-unity",
-  "math-extension-functions"
+  "math-extension-functions",
+  "single-responsibility-principle"
 ]
 
 export default RegisteredArticles;
