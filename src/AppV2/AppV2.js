c0f783757a318b32aba7fa2680f368ac5081381f
Bruno <b.b.da.costa@gmail.com>
2023-06-08 11:48:28 -0300

    ♻️ Group theme properties to improve configuration

--- src/AppV2/AppV2.js
+++ src/AppV2/AppV2.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { NavBar } from './Navbar/NavBar';
 import Footer from './Footer/Footer';
 import Home from './Home/Home';
+import style from './AppV2.module.css'
 
 export default function AppV2() {
   return (
-    <div style={{ backgroundColor: "#EEEDF4" }}>
+    <div className={style.app}>
       <NavBar />
       <Home />
       <Footer />
