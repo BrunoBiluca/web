f2be4113696d8fb5ea4590637d1eff63b30d3f61
Bruno <b.b.da.costa@gmail.com>
2023-06-11 15:50:42 -0300

    ✨ Add smooth scroll

--- src/AppV2/AppV2.js
+++ src/AppV2/AppV2.js
@@ -3,13 +3,16 @@ import { NavBar } from './Navbar/NavBar';
 import Footer from './Footer/Footer';
 import Home from './Home/Home';
 import style from './AppV2.module.css'
+import SmoothScroll from './Components/SmoothScroll/SmoothScroll';
 
 export default function AppV2() {
   return (
     <div className={style.app}>
-      <NavBar />
-      <Home />
-      <Footer />
+      <SmoothScroll>
+        <NavBar />
+        <Home />
+        <Footer />
+      </SmoothScroll>
     </div>
   );
 }
